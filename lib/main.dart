8ba3c97bd7cea2fa91173ffa2fa237e252730919
chuanjie <63796959+poppet-t@users.noreply.github.com>
2023-11-26 18:24:21 +0800

    name
    name

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'che_ese',
       theme: ThemeData(
           textTheme: Theme.of(context)
               .textTheme
